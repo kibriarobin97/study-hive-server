bd255c03cb63433879c8cad2bf8f5d339093903f
kibriarobin97 <kibriarobin97@gmail.com>
2024-06-04 15:53:03 +0600

    get my enroll class api

--- index.js
+++ index.js
@@ -311,6 +311,13 @@ async function run() {
     res.send(result)
   })
 
+  app.get('/my-enroll-class/:email', async(req, res) => {
+    const email = req.params.email;
+    const query = {email: email}
+    const result = await enrollClassCollection.find(query).toArray()
+    res.send(result)
+  })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
